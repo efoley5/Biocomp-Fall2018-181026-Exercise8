65b7a04c561707a25a242eb478c71b02bf5c15b7
Ellen Foley <efoley5@nd.edu>
2018-11-02 08:51:55 -0400

    Question 1 Annotations and Question 2

--- Exercise8.R
+++ Exercise8.R
@@ -1,19 +1,44 @@
 #read document as a tables
 game=read.table("UWvMSU_1-22-13.txt",stringsAsFactors = F, header = T)
 head(game)
+#make a new data frame with the cumulative scores 
 both_teams_total=data.frame(UW=rep(NA,50), MSU=rep(NA, 50))
 totalScoreUW<-0
 totalScoreMSU<-0
+#Get the cumulative scores for UW and add it to the new dataframe 
 for (i in 1:nrow(game)) {
   if (game[i,2] =="UW") {
     totalScoreUW = totalScoreUW + game[i,3]
     both_teams_total[i,1]=totalScoreUW
     both_teams_total[i,2]=totalScoreMSU
+#Get the cumulative scores for MSU and add it to the new dataframe 
   }else{
     totalScoreMSU = totalScoreMSU + game[i,3]
     both_teams_total[i,1]=totalScoreUW
     both_teams_total[i,2]=totalScoreMSU
   }
 }
+#Make a plot of MSU in green and UW in black 
 plot(x=game$time, y=both_teams_total$UW, type = "l", xlab = "Time (minutes)", ylab = "Total points", col="black")
 lines(game$time, both_teams_total$MSU, type = "l", col="green")
+
+
+#Question 2 
+
+
+number_guess <- sample(1:100, 1) 
+guess <- function(guess){
+  if (guess > number_guess){
+    print("Lower")
+  }else{ 
+    if (guess < number_guess){
+      print("Higher")
+    }else{ 
+      if (guess == number_guess)
+        print( "Correct!")
+      
+    }
+  }
+}
+guess(7)
+guess(8)
